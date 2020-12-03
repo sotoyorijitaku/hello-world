444731bafc93a42c82ac3e95b2bae4016933ffc3
Sotoyori Jitaku <sotoyorijitaku@gmail.com>
2020-12-03 23:30:34 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <br />
+          Hello world
         </p>
         <a
           className="App-link"
